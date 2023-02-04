cc0cb5d81874ea36eb3ad7501318dc10c5b3b106
IntroSpace <introspace321@gmail.com>
2023-02-04 18:19:49 +0300

    ьфкы

--- mars.py
+++ mars.py
@@ -31,5 +31,27 @@ def promotion():
                 </html>"""
 
 
+@app.route('/image_sample')
+def image():
+    return f'''	<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                   </head>
+                    <body>
+                        <link rel="stylesheet" type="text/css"
+                         href="{url_for('static',
+                                        filename='css/style.css')}" />
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.jpg')}" 
+                               alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-primary" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
